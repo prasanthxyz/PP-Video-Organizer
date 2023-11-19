49854adb2dfe9973c33b17f2e05a1af146c83d98
Prasanth P <prasanthxyz@gmail.com>
2023-11-19 23:42:40 +0530

    Add video player and keyboard seek support

--- src/renderer/src/pages/Video.jsx
+++ src/renderer/src/pages/Video.jsx
@@ -1,10 +1,30 @@
+import { Button, Spinner, Tab, TabList, TabPanel, TabPanels, Tabs, VStack } from '@chakra-ui/react'
 import * as React from 'react'
 import { useParams } from 'react-router'
+import mainAdapter from '../../../mainAdapter'
+import VideoPlayer from '../components/VideoPlayer'
 
 export default function Video() {
-  let { videoPath } = useParams()
-  const videoPathComponents = videoPath.replace(/\\/g, '/').split('/')
+  const [tgpExists, setTgpExists] = React.useState(false)
+  const [isGeneratingTgp, setIsGeneratingTgp] = React.useState(false)
 
+  let { videoPath } = useParams()
+  const setFilesExist = async () => {
+    setTgpExists(await mainAdapter.isTgpExisting(videoPath))
+  }
+  React.useEffect(() => {
+    setFilesExist()
+  }, [])
+
+  const handleGenerateTgp = async (regenerate = false) => {
+    setIsGeneratingTgp(true)
+    await mainAdapter.generateTgp(videoPath, regenerate)
+    setTgpExists(true)
+    setIsGeneratingTgp(false)
+  }
+
+  const videoPathComponents = videoPath.replace(/\\/g, '/').split('/')
+  const videoName = videoPathComponents[videoPathComponents.length - 1]
   const getImgPath = () => {
     const imgPathComponents = videoPathComponents.slice(0, videoPathComponents.length - 1)
     imgPathComponents.push('img')
@@ -12,6 +32,31 @@ export default function Video() {
     return imgPathComponents.join('/')
   }
 
+  const tgpButton = isGeneratingTgp ? (
+    <Spinner />
+  ) : (
+    <Button onClick={async () => await handleGenerateTgp(tgpExists)}>
+      {tgpExists ? 'Regenerate' : 'Generate'} TGP
+    </Button>
+  )
+
   const imgPath = getImgPath()
-  return <img src={`file:///${imgPath}`} />
+  return (
+    <VStack>
+      <h3>{videoName}</h3>
+      <Tabs>
+        <TabList>
+          <Tab>TGP</Tab>
+          <Tab>Video</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>{tgpExists ? <img src={`file:///${imgPath}`} /> : <>TGP MISSING</>}</TabPanel>
+          <TabPanel>
+            <VideoPlayer autoplay={false} controls={true} sources={videoPath} />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+      {tgpButton}
+    </VStack>
+  )
 }
