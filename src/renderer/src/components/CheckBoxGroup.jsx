ae3e146975a60076c5ea472884d66f436e585455
Prasanth P <prasanthxyz@gmail.com>
2023-11-26 23:05:23 +0530

    Refactor CheckBoxGroup

--- src/renderer/src/components/CheckBoxGroup.jsx
+++ src/renderer/src/components/CheckBoxGroup.jsx
@@ -2,26 +2,24 @@ import * as React from 'react'
 import { Button, Form, InputGroup, Spinner, Stack } from 'react-bootstrap'
 
 export default function CheckBoxGroup({ allItems, selectedItems, update }) {
-  const [value, setValue] = React.useState(new Set())
-
-  React.useEffect(() => {
-    setValue(selectedItems)
-  }, [selectedItems])
-
   const handleUpdate = async (e) => {
-    const newValue = new Set(value)
+    const newValue = new Set(selectedItems)
     if (e.target.checked) {
       newValue.add(e.target.value)
     } else {
       newValue.delete(e.target.value)
     }
-    setValue(newValue)
     await update(newValue)
   }
 
   const checkBoxes = allItems.map((item) => (
     <div key={item}>
-      <Form.Check value={item} label={item} onChange={handleUpdate} checked={value.has(item)} />
+      <Form.Check
+        value={item}
+        label={item}
+        onChange={handleUpdate}
+        checked={selectedItems.has(item)}
+      />
     </div>
   ))
 
