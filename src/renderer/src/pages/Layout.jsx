0c2d6b8802ac829e46c359ad56961d339bea2910
Prasanth P <prasanthxyz@gmail.com>
2023-12-04 03:21:07 +0530

    Fix Navbar styling and add Dark Mode

--- src/renderer/src/pages/Layout.jsx
+++ src/renderer/src/pages/Layout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Button, Container, Nav, Navbar, Stack } from 'react-bootstrap'
-import { ArrowLeft } from 'react-bootstrap-icons'
+import { Button, Container, Nav, Navbar } from 'react-bootstrap'
+import { ArrowLeft, ToggleOff, ToggleOn } from 'react-bootstrap-icons'
 import { useHotkeys } from 'react-hotkeys-hook'
 import { Outlet, useNavigate } from 'react-router-dom'
-import logo from '../assets/icon.png'
+import lightIcon from '../assets/lighticon.png'
+import darkIcon from '../assets/darkicon.png'
 
 export default function Layout() {
+  const [isDarkMode, setIsDarkMode] = React.useState(false)
+  const [activeNav, setActiveNav] = React.useState('Random')
+
   const navigate = useNavigate()
 
   useHotkeys('Shift+R', () => navigate('/'))
@@ -13,32 +17,74 @@ export default function Layout() {
   useHotkeys('Shift+G', () => navigate('/galleries'))
   useHotkeys('Shift+T', () => navigate('/tags'))
 
+  const updateNav = (e) => setActiveNav(e.target.text)
+
+  const toggleDarkMode = () => {
+    if (isDarkMode) {
+      document.documentElement.removeAttribute('data-bs-theme')
+    } else {
+      document.documentElement.setAttribute('data-bs-theme', 'dark')
+    }
+    setIsDarkMode(!isDarkMode)
+  }
+
   return (
-    <Container fluid>
-      <Navbar className="bg-body-tertiary">
+    <>
+      <Navbar className="bg-body-secondary px-3">
         <Navbar.Brand>
-          <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="PVORG" />
+          <img
+            src={isDarkMode ? darkIcon : lightIcon}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="PVORG"
+          />
           &nbsp;&nbsp;PVORG
         </Navbar.Brand>
-        <Stack direction="horizontal" className="me-auto ms-3">
-          <Nav.Link className="me-2" href="#/">
+        <Nav className="justify-content-end flex-grow-1 pe-3">
+          <Nav.Link className="me-2" onClick={updateNav} active={activeNav === 'Random'} href="#/">
             Random
           </Nav.Link>
-          <Nav.Link className="me-2" href="#/videos">
+          <Nav.Link
+            className="me-2"
+            onClick={updateNav}
+            active={activeNav === 'Videos'}
+            href="#/videos"
+          >
             Videos
           </Nav.Link>
-          <Nav.Link className="me-2" href="#/galleries">
+          <Nav.Link
+            className="me-2"
+            onClick={updateNav}
+            active={activeNav === 'Galleries'}
+            href="#/galleries"
+          >
             Galleries
           </Nav.Link>
-          <Nav.Link className="me-2" href="#/tags">
+          <Nav.Link
+            className="me-2"
+            onClick={updateNav}
+            active={activeNav === 'Tags'}
+            href="#/tags"
+          >
             Tags
           </Nav.Link>
-        </Stack>
-        <Button size="sm" variant="dark" className="d-flex" onClick={() => navigate(-1)}>
+        </Nav>
+        <Button size="sm" className="d-flex me-2" onClick={() => navigate(-1)}>
           <ArrowLeft />
         </Button>
+        <Button
+          size="sm"
+          variant={isDarkMode ? 'light' : 'dark'}
+          className="d-flex"
+          onClick={toggleDarkMode}
+        >
+          {isDarkMode ? <ToggleOn /> : <ToggleOff />}
+        </Button>
       </Navbar>
-      <Outlet />
-    </Container>
+      <Container fluid>
+        <Outlet />
+      </Container>
+    </>
   )
 }
