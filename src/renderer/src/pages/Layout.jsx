8ebb1a9d9edfb6cfd063871d3c4f9f2ce8b4fe16
Prasanth P <prasanthxyz@gmail.com>
2023-12-09 16:07:55 +0530

    Improve navbar management

--- src/renderer/src/pages/Layout.jsx
+++ src/renderer/src/pages/Layout.jsx
@@ -2,22 +2,40 @@ import React from 'react'
 import { Button, Container, Nav, Navbar } from 'react-bootstrap'
 import { ArrowLeft, ToggleOff, ToggleOn } from 'react-bootstrap-icons'
 import { useHotkeys } from 'react-hotkeys-hook'
-import { Outlet, useNavigate } from 'react-router-dom'
-import lightIcon from '../assets/lighticon.png'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import darkIcon from '../assets/darkicon.png'
+import lightIcon from '../assets/lighticon.png'
+
+const PAGES = [
+  { text: 'Random', shortcut: 'Shift+R', location: '/', prefix: '/' },
+  { text: 'Videos', shortcut: 'Shift+V', location: '/videos', prefix: '/video' },
+  { text: 'Galleries', shortcut: 'Shift+G', location: '/galleries', prefix: '/galler' },
+  { text: 'Tags', shortcut: 'Shift+T', location: '/tags', prefix: '/tag' }
+]
+
+const getActiveNav = (location) => {
+  for (let i = 1; i < PAGES.length; i++) {
+    if (location.startsWith(PAGES[i].prefix)) return PAGES[i].text
+  }
+  return 'Random'
+}
 
 export default function Layout() {
+  const location = useLocation().pathname
   const [isDarkMode, setIsDarkMode] = React.useState(false)
-  const [activeNav, setActiveNav] = React.useState('Random')
+  const [activeNav, setActiveNav] = React.useState(getActiveNav(location))
+
+  React.useEffect(() => {
+    setActiveNav(getActiveNav(location))
+  }, [location])
 
   const navigate = useNavigate()
 
-  useHotkeys('Shift+R', () => navigate('/'))
-  useHotkeys('Shift+V', () => navigate('/videos'))
-  useHotkeys('Shift+G', () => navigate('/galleries'))
-  useHotkeys('Shift+T', () => navigate('/tags'))
-
-  const updateNav = (e) => setActiveNav(e.target.text)
+  for (const page of Object.values(PAGES)) {
+    useHotkeys(page.shortcut, () => {
+      navigate(page.location)
+    })
+  }
 
   const toggleDarkMode = () => {
     if (isDarkMode) {
@@ -42,33 +60,16 @@ export default function Layout() {
           &nbsp;&nbsp;PVORG
         </Navbar.Brand>
         <Nav className="justify-content-end flex-grow-1 pe-3">
-          <Nav.Link className="me-2" onClick={updateNav} active={activeNav === 'Random'} href="#/">
-            Random
-          </Nav.Link>
-          <Nav.Link
-            className="me-2"
-            onClick={updateNav}
-            active={activeNav === 'Videos'}
-            href="#/videos"
-          >
-            Videos
-          </Nav.Link>
-          <Nav.Link
-            className="me-2"
-            onClick={updateNav}
-            active={activeNav === 'Galleries'}
-            href="#/galleries"
-          >
-            Galleries
-          </Nav.Link>
-          <Nav.Link
-            className="me-2"
-            onClick={updateNav}
-            active={activeNav === 'Tags'}
-            href="#/tags"
-          >
-            Tags
-          </Nav.Link>
+          {PAGES.map((page) => (
+            <Nav.Link
+              key={page.text}
+              className="me-2"
+              active={activeNav === page.text}
+              href={`#${page.location}`}
+            >
+              {page.text}
+            </Nav.Link>
+          ))}
         </Nav>
         <Button size="sm" className="d-flex me-2" onClick={() => navigate(-1)}>
           <ArrowLeft />
