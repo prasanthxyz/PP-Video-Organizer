d13b6593ee226f6b0d4eee0ad3bb4a1878e4a3f1
Prasanth Presennan <ppresenn@cisco.com>
2024-05-31 02:01:33 +0530

    Add react-query devtools

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import App from './App'
 
 import 'rsuite/dist/rsuite.min.css'
@@ -12,6 +13,7 @@ const queryClient = new QueryClient()
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
