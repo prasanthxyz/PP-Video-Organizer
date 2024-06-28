2563c97175525a93fe0f69521792e37818508aaa
Prasanth Presennan <ppresenn@cisco.com>
2024-06-28 13:03:44 +0530

    Add cors to the koa server

--- src/main/server/index.ts
+++ src/main/server/index.ts
@@ -1,3 +1,4 @@
+import cors from '@koa/cors'
 import Koa from 'koa'
 import parser from 'koa-bodyparser'
 import router from './routes'
@@ -6,5 +7,5 @@ import { setupDB } from './utils/data'
 const app = new Koa()
 export const startBackendServer = (): void => {
   setupDB()
-  app.use(parser()).use(router.routes()).listen(3000)
+  app.use(parser()).use(cors()).use(router.routes()).listen(3000)
 }
